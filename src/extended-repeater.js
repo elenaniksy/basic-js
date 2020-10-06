dadc7e00ef42a6882438b773d253584031fa3d58
elenaniksy <elfhelen@gmail.com>
2020-10-06 19:15:29 +0300

    feat: realized basic functional to extended-repeater and transform-array tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const toStr = (s) => {
+    if (typeof s === 'string') {
+      return s;
+    } else {
+      return String(s);
+    }
+  }
+
+  let incomingStr = toStr(str);
+  let strAddition = [];
+  let result = [];
+
+  const formStrAddition = () => {
+    strAddition.push(incomingStr);
+    if(options.additionRepeatTimes && typeof options.additionRepeatTimes === 'number') {
+      for(let i = 0; i <options.additionRepeatTimes; i++) {
+        if(options.addition) {
+          strAddition.push(toStr(options.addition));
+        }
+      }
+      if (options.additionSeparator) {
+        return strAddition.join(options.additionSeparator);
+      } else {
+        return strAddition.join('');
+      }
+    } else {
+      if(options.addition) {
+        strAddition.push(toStr(options.addition));
+      }
+      return strAddition.join('');
+    }
+  };
+
+  if(options) {
+    const strToReplicate = formStrAddition();
+    if(options.repeatTimes && typeof options.repeatTimes === 'number') {
+      for (let i = 0; i < options.repeatTimes; i++) {
+        result.push(strToReplicate);
+      }
+    } else {
+      result.push(strToReplicate);
+    }
+
+    if (options.separator) {
+      return result.join(options.separator);
+    } else {
+      return result.join('+');
+    }
+
+  } else {
+    return incomingStr;
+  }
 };
-  
+
+
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const result = [];
+
+  if(Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i] === '--discard-next') {
+        result.shift(arr[i+1]);
+      } else if (arr[i] === '--discard-prev') {
+        result.shift(arr[i-1]);
+      } else if (arr[i] === '--double-next') {
+        result.push(arr[i]);
+        result.push(arr[i+1]);
+      } else if (arr[i] === '--double-prev') {
+        result.push(arr[i]);
+        result.push(arr[i+1]);
+      } else {
+        result.push(arr[i]);
+      }
+    }
+  } else {
+    return [];
+  }
+ return result;
 };
