3513f5add46ea3d3e77816166615e3e63ce5b15a
elenaniksy <elfhelen@gmail.com>
2020-10-27 16:36:51 +0300

    feat: add solution to vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+    this.alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
   }
+
+  encrypt(m, k) {
+    const message = [...m.toUpperCase()];
+    const keys = [...k.toUpperCase()];
+    let position = 0;
+    let result = message.map( item => {
+      if (this.alphabet.includes(item)) {
+        let indexOfItem = (this.alphabet.indexOf(keys[position]) + this.alphabet.indexOf(item)) % this.alphabet.length;
+        position = (position + 1) % keys.length;
+        return this.alphabet[indexOfItem];
+      }
+      return item;
+    } );
+    return this.mode ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(m, k) {
+    const message = [...m.toUpperCase()];
+    const keys = [...k.toUpperCase()];
+    let position = 0;
+    let result = message.map( item => {
+      if (this.alphabet.includes(item)) {
+        let indexOfItem = this.alphabet.indexOf(item) - this.alphabet.indexOf(keys[position]);
+        if (indexOfItem < 0) indexOfItem += this.alphabet.length;
+        position = (position + 1) % keys.length;
+        return this.alphabet[indexOfItem];
+      }
+      return item;
+    } );
+    return this.mode ? result.join('') : result.reverse().join('');
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
